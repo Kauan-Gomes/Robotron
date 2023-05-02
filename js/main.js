6859326220239443188d6ba60cbd3a33f87ab448
Kauan Gomes <Kauang13@gmail.com>
2023-05-02 16:10:42 -0300

    ajustando dinamicamente o somar e subtrair

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 
 
-const robotron = document.querySelector('#robotron')
-robotron.addEventListener('click', dizOi)
+const controle = document.querySelectorAll('.controle-ajuste')
 
-function dizOi(){
-    console.log('oi')
+controle.forEach ((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode);
+
+    })
+})
+
+
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector('.controle-contador');
+    
+    // ParseiInt servre para converter o valor para Inteiro ao inves de String
+    if (operacao === "-") {
+        peca.value = parseInt(peca.value) - 1
+    } else {
+        peca.value = parseInt(peca.value) + 1
+    }
 }
+
