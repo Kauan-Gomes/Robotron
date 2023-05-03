8c4d286a4e247cd9588a9519dffd6b65dda0f5bc
Kauan Gomes <Kauang13@gmail.com>
2023-05-03 17:01:12 -0300

    robotron finalizado

--- js/main.js
+++ js/main.js
@@ -1,17 +1,51 @@
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+const controle = document.querySelectorAll('[data-controle]')
+const estatistica = document.querySelectorAll('[data-estatistica]')
 
 
-const controle = document.querySelectorAll('.controle-ajuste')
 
 controle.forEach ((elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent, evento.target.parentNode);
-
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
+        atualizaEstatistica(evento.target.dataset.peca)
     })
 })
 
 
 function manipulaDados(operacao, controle) {
-    const peca = controle.querySelector('.controle-contador');
+    const peca = controle.querySelector('[data-contador]');
     
     // ParseiInt servre para converter o valor para Inteiro ao inves de String
     if (operacao === "-") {
@@ -21,3 +55,10 @@ function manipulaDados(operacao, controle) {
     }
 }
 
+function atualizaEstatistica(peca) {
+    console.log(pecas[peca])
+
+    estatistica.forEach( (elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    })
+}
